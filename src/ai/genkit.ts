e9895114f3ca8a80ce35156624425e85a7da1868
Yashasvini Bhanuraj <97218504+yashasvini2003@users.noreply.github.com>
2025-08-13 22:57:36 -0400

    delete genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,1 @@
-import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
 
-export const ai = genkit({
-  plugins: [googleAI()],
-  model: 'googleai/gemini-1.5-flash-latest',
-});
